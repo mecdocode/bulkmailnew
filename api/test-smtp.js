6114c23f41afa7cb38ba21239af1dec78d5f53e4
mecdocode <meccode360@gmail.com>
2025-09-02 21:34:07 +0530

    fix: correct API function export and vercel config

--- api/test-smtp.js
+++ api/test-smtp.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer')
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
