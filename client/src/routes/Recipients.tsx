2da82b1d211189e3939cee74ed306888bc71d7e8
mecdocode <meccode360@gmail.com>
2025-09-02 15:36:30 +0530

    your message about the changes

--- client/src/routes/Recipients.tsx
+++ client/src/routes/Recipients.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Upload, Users, ArrowRight, ArrowLeft, AlertCircle } from 'lucide-react'
 import { WizardStepper } from '../components/WizardStepper'
